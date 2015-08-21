407526ab5197f70251539fd50b9d4549934efcef
Connor Doyle <connor@mesosphere.io>
2015-08-21 12:47:32 -0700

    Enable cpp compilation in build.rs.

--- build.rs
+++ build.rs
@@ -1,10 +1,9 @@
 extern crate gcc;
 
 fn main() {
-    let sources = &[
-        "resources/mesos-c/utils.hpp",
-        "resources/mesos-c/scheduler_driver.cpp",
-    ];
-
-    gcc::compile_library("libmesosc.a", sources);
+    gcc::Config::new()
+        .cpp(true)
+        .file("resources/mesos-c/scheduler_driver.cpp")
+        .include("resources/mesos-c")
+        .compile("libmesosc.a");
 }
